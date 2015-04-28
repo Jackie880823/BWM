2a5291126e1c5763b2467531dfedf1635797be08
wing <wingzhong@madxstudio.cn>
2015-04-28 10:12:31 +0800

    文字

--- app/src/main/java/com/madx/bwm/ui/WallFragment.java
+++ app/src/main/java/com/madx/bwm/ui/WallFragment.java
@@ -171,7 +171,6 @@ public class WallFragment extends BaseFragment<MainActivity> implements WallAdap
                 //gsonb.registerTypeAdapter(Date.class, ds);
                 Gson gson = gsonb.create();
                 try {
-                    Log.d("", "2memberId============" + response);
                     data = gson.fromJson(response, new TypeToken<ArrayList<WallEntity>>() {
                     }.getType());
                     if (isRefresh) {
