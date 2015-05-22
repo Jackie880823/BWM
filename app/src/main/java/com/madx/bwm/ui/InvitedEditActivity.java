b6d526059cc12d50dd780468e1f7170283b7bd6f
wing <wingzhong@madxstudio.cn>
2015-05-22 10:29:06 +0800

    去掉不必要重写changeTitleColor;调整通知小图标，不同类型不同icon标识

--- app/src/main/java/com/madx/bwm/ui/InvitedEditActivity.java
+++ app/src/main/java/com/madx/bwm/ui/InvitedEditActivity.java
@@ -45,11 +45,6 @@ public class InvitedEditActivity extends BaseActivity {
     }
 
     @Override
-    protected void changeTitleColor(int color) {
-        super.changeTitleColor(color);
-    }
-
-    @Override
     protected void setTitle() {
         tvTitle.setText(R.string.title_invited_status);
     }
