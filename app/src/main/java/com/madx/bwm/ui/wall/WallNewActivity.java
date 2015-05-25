de61870bf73ab7e0ed08c93ff4d8b4f512e5aa6e
jackie <zhuweiping@madxstudio.cn>
2015-05-25 15:28:46 +0800

    Fix  点击左上角返回键关闭Activity同时关闭键盘

--- app/src/main/java/com/madx/bwm/ui/wall/WallNewActivity.java
+++ app/src/main/java/com/madx/bwm/ui/wall/WallNewActivity.java
@@ -3,7 +3,6 @@ package com.madx.bwm.ui.wall;
 import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.view.View;
-import android.view.WindowManager;
 
 import com.madx.bwm.R;
 import com.madx.bwm.ui.BaseActivity;
@@ -13,11 +12,8 @@ import com.madx.bwm.ui.BaseActivity;
  */
 public class WallNewActivity extends BaseActivity {
 
-
     @Override
     protected void initBottomBar() {
-        // 隐藏键盘
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE|WindowManager.LayoutParams.SOFT_INPUT_ADJUST_UNSPECIFIED|WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
 
     @Override
