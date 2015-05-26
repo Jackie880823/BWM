c9083e0a54a42b26dd0824430e67c7cef0d64e21
wing <wingzhong@madxstudio.cn>
2015-05-26 18:02:24 +0800

    调整主页tab位置

--- app/src/main/java/com/madx/bwm/ui/MainActivity.java
+++ app/src/main/java/com/madx/bwm/ui/MainActivity.java
@@ -1,8 +1,6 @@
 package com.madx.bwm.ui;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +8,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -26,6 +25,7 @@ import com.madx.bwm.entity.UserEntity;
 import com.madx.bwm.ui.wall.WallFragment;
 import com.madx.bwm.ui.wall.WallNewActivity;
 import com.madx.bwm.util.FileUtil;
+import com.madx.bwm.util.PreferencesUtil;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -85,7 +85,6 @@ public class MainActivity extends BaseActivity {
     private static MainActivity mainActivityInstance;
     private String leaveGroup;
     public static final String LAST_LEAVE_INDEX = "lastLeaveIndex";
-    public static String SAVE_FILE_NAME = "leaveTab";
     private int leavePagerIndex = 0;
 
     @Override
@@ -98,6 +97,22 @@ public class MainActivity extends BaseActivity {
         mainActivityInstance = this;
     }
 
+    @Override
+    protected void initTitleBar() {
+        super.initTitleBar();
+
+
+        //for default
+        changeTitleColor(R.color.tab_color_press5);
+        changeTitle(R.string.title_tab_my_family);
+        leftButton.setVisibility(View.INVISIBLE);
+        rightButton.setVisibility(View.VISIBLE);
+        tabIv0.setImageResource(R.drawable.tab_family_select);
+        ivTab0.setBackgroundColor(getResources().getColor(R.color.tab_color_press5));
+        tabTv0.setTextColor(Color.BLACK);
+        //for last tab
+        mViewPager.setCurrentItem(leavePagerIndex);
+    }
 
     @Override
     protected void onPause() {
@@ -111,6 +126,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void onResume() {
+
         super.onResume();
         if (fragments != null) {
             for (Fragment f : fragments) {
@@ -120,15 +136,15 @@ public class MainActivity extends BaseActivity {
 //        if(currentTabEnum!=null){
 //            changeTitleColor();
 //        }
+
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        SharedPreferences sp = getSharedPreferences(SAVE_FILE_NAME, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putInt(LAST_LEAVE_INDEX, mViewPager.getCurrentItem());
-        editor.commit();
+        Log.i("", "leavePagerIndex===========" + currentTabEnum.ordinal());
+        PreferencesUtil.saveValue(this, LAST_LEAVE_INDEX, currentTabEnum.ordinal());
+//        editor.putInt(LAST_LEAVE_INDEX, mViewPager.getCurrentItem());
     }
 
     public static UserEntity getUser() {
@@ -266,11 +282,11 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        SAVE_FILE_NAME=MainActivity.getUser().getUser_id()+SAVE_FILE_NAME;
         mViewPager = getViewById(R.id.pager);
         fragments = new ArrayList<>();
 //        WallFragment wallFragment = WallFragment.newInstance();
         fragments.add(FamilyFragment.newInstance());
+        fragments.add(MessageMainFragment.newInstance());
         fragments.add(WallFragment.newInstance());
         fragments.add(EventFragment.newInstance());
 //        eventFragment = EventFragment.newInstance();
@@ -286,7 +302,6 @@ public class MainActivity extends BaseActivity {
 ////            fragments.add(1,eventStartupFragment);
 //        }
 //        fragments.add(MessageFragment.newInstance());
-        fragments.add(MessageMainFragment.newInstance());
         fragments.add(MoreFragment.newInstance());
 
 
@@ -301,20 +316,22 @@ public class MainActivity extends BaseActivity {
 
         mViewPager.setOffscreenPageLimit(0);
 
-        SharedPreferences sp = getSharedPreferences(SAVE_FILE_NAME, Context.MODE_PRIVATE);
-        leavePagerIndex = sp.getInt(LAST_LEAVE_INDEX, 0);
 
+        leavePagerIndex = PreferencesUtil.getValue(this, LAST_LEAVE_INDEX, 0);
         leaveGroup = getIntent().getStringExtra("leaveGroup");
         if ("leaveGroup".equals(leaveGroup)) {
             leavePagerIndex = 3;
         }
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                addStickerList();
-                addImageList();
-            }
-        }).start();
+
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                addStickerList();
+//                addImageList();
+//            }
+//        }).start();
+
+
     }
 
     static List<String> STICKER_NAME_LIST = new ArrayList<>();
@@ -365,39 +382,6 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
-    protected void initTitleBar() {
-        super.initTitleBar();
-        TabEnum tab;
-        switch (leavePagerIndex) {
-            case 0:
-                tab = TabEnum.family;
-                break;
-            case 1:
-                tab = TabEnum.wall;
-                break;
-            case 2:
-                tab = TabEnum.event;
-                break;
-            case 3:
-                tab = TabEnum.chat;
-                break;
-            case 4:
-                tab = TabEnum.more;
-                break;
-            default:
-                tab = TabEnum.family;
-                break;
-        }
-        changeTab(tab);//默认第一个
-        mViewPager.setCurrentItem(leavePagerIndex);
-//        if ("leaveGroup".equals(leaveGroup)) {
-//            mViewPager.setCurrentItem(3);
-//        } else {
-//            changeTab(TabEnum.family);//默认第一个
-//        }
-    }
-
-    @Override
     public void requestData() {
 
     }
@@ -429,7 +413,6 @@ public class MainActivity extends BaseActivity {
         ivTab4.setBackgroundColor(getResources().getColor(R.color.transparent_color));
 
     }
-
 
     protected void changeTab(TabEnum tabEnum) {
         switch (tabEnum) {
@@ -547,7 +530,7 @@ public class MainActivity extends BaseActivity {
     }
 
     public enum TabEnum {
-        family, wall, event, chat, more;
+        family, chat,wall, event, more;
     }
 
     SnackBar snackBar;
