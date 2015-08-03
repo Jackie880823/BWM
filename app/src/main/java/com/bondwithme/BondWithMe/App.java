9cc3af73495490a4283c26f5df2cbab198c4890f
wing <wingzhong@madxstudio.cn>
2015-08-03 17:36:42 +0800

    去掉更新测试代码

--- app/src/main/java/com/bondwithme/BondWithMe/App.java
+++ app/src/main/java/com/bondwithme/BondWithMe/App.java
@@ -115,8 +115,8 @@ public class App extends MultiDexApplication {
                 LogUtil.e("", "response===========" + response);
                 try {
                     JSONObject object = new JSONObject(response);
-                    if (!("" + AppInfoUtil.getAppVersionCode(context)).equals(object.get("app_latest_version")) ) {
-//                    if (!("" + AppInfoUtil.getAppVersionCode(context)).equals(object.get("app_latest_version")) && object.get("app_major_update") == "1") {
+//                    if (!("" + AppInfoUtil.getAppVersionCode(context)).equals(object.get("app_latest_version")) ) {
+                    if (!("" + AppInfoUtil.getAppVersionCode(context)).equals(object.get("app_latest_version")) && object.get("app_major_update") == "1") {
                         //must update app
                         needUpdate = true;
                         showUpdateDialog(context);
