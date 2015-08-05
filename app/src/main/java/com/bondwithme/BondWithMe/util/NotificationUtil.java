4c36201554570ec69214deae77451160b4a90939
wing <wingzhong@madxstudio.cn>
2015-08-05 19:31:38 +0800

    使异常报告和notification的Activity 为nohistory模式

--- app/src/main/java/com/bondwithme/BondWithMe/util/NotificationUtil.java
+++ app/src/main/java/com/bondwithme/BondWithMe/util/NotificationUtil.java
@@ -284,6 +284,7 @@ public class NotificationUtil {
 //            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 //            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.putExtra(BaseActivity.IS_OUTSIDE_INTENT,true);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
             contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         }
         return contentIntent;
