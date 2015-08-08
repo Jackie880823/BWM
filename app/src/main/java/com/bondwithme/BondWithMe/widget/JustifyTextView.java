4143da4f6009fb96022c834b4cffcc6dfb27e6b6
quan <quankun@madxstudio.cn>
2015-08-08 18:03:00 +0800

    解决消息把英文这个单词拆开显示的问题

---
+++ app/src/main/java/com/bondwithme/BondWithMe/widget/JustifyTextView.java
@@ -0,0 +1,182 @@
+package com.bondwithme.BondWithMe.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.ContextMenu;
+import android.widget.TextView;
+
+import com.bondwithme.BondWithMe.util.LogUtil;
+
+/**
+ * Created by quankun on 15/8/8.
+ */
+public class JustifyTextView extends TextView {
+
+    private int mLineY;
+    private float mViewWidth;
+    private float paddingLeft;
+    private float paddingRight;
+    public static final String TWO_CHINESE_BLANK = "  ";
+
+    public JustifyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paddingLeft = this.getPaddingLeft();
+        paddingRight = this.getPaddingRight();
+    }
+
+    private static final String TAG = JustifyTextView.class.getSimpleName();
+
+    public JustifyTextView(Context context) {
+        super(context);
+    }
+
+    public JustifyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public JustifyTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /**
+     * <br>记录最后一次点击是否为长按，部分手机长按后还会调用{@link #performClick()},设置此标识位为在
+     * <br>长按当前View后值设置为<code>true</code>, 调用{@link #performClick()}时不禁用选择复制功能
+     */
+    boolean lastClickIsLong = false;
+
+    @Override
+    public boolean performLongClick() {
+        LogUtil.i(TAG, "performLongClick");
+        lastClickIsLong = true;
+        boolean handled = super.performLongClick();
+        return handled;
+    }
+
+    @Override
+    public boolean performClick() {
+        LogUtil.i(TAG, "performClick");
+        if(!lastClickIsLong) {
+            setTextIsSelectable(false);
+        }
+        lastClickIsLong = false;
+        boolean result = super.performClick();
+        setTextIsSelectable(true);
+        return result;
+    }
+
+    /**
+     * Views should implement this if the view itself is going to add items to
+     * the context menu.
+     *
+     * @param menu the context menu to populate
+     */
+    @Override
+    protected void onCreateContextMenu(ContextMenu menu) {
+        super.onCreateContextMenu(menu);
+    }
+
+    @Override
+    public void setOnClickListener(OnClickListener l) {
+        super.setOnClickListener(l);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right,
+                            int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        TextPaint paint = getPaint();
+        paint.setColor(getCurrentTextColor());
+        paint.drawableState = getDrawableState();
+        mViewWidth = getMeasuredWidth() - paddingRight - paddingLeft;
+        String text = getText().toString();
+        mLineY = 0;
+        mLineY += getTextSize() * 1.4f;
+        Layout layout = getLayout();
+
+        // layout.getLayout()在4.4.3出现NullPointerException
+        if (layout == null) {
+            return;
+        }
+
+        Paint.FontMetrics fm = paint.getFontMetrics();
+
+        int textHeight = (int) (Math.ceil(fm.descent - fm.ascent));
+        textHeight = (int) (textHeight * layout.getSpacingMultiplier() + layout
+                .getSpacingAdd());
+        //解决了最后一行文字间距过大的问题
+        for (int i = 0; i < layout.getLineCount(); i++) {
+            int lineStart = layout.getLineStart(i);
+            int lineEnd = layout.getLineEnd(i);
+            float width = StaticLayout.getDesiredWidth(text, lineStart,
+                    lineEnd, getPaint());
+            String line = text.substring(lineStart, lineEnd);
+
+            if (i < layout.getLineCount() - 1) {
+                if (needScale(line)) {
+                    drawScaledText(canvas, lineStart, line, width);
+                } else {
+                    canvas.drawText(line, paddingLeft, mLineY, paint);
+                }
+            } else {
+                canvas.drawText(line, paddingLeft, mLineY, paint);
+            }
+            mLineY += textHeight;
+        }
+    }
+
+    private void drawScaledText(Canvas canvas, int lineStart, String line, float lineWidth) {
+        float x = 0;
+        if (isFirstLineOfParagraph(lineStart, line)) {
+            String blanks = "  ";
+            canvas.drawText(blanks, x + paddingLeft, mLineY, getPaint());
+            float bw = StaticLayout.getDesiredWidth(blanks, getPaint());
+            x += bw;
+
+            line = line.substring(3);
+        }
+
+        int gapCount = line.length() - 1;
+        int i = 0;
+        if (line.length() > 2 && line.charAt(0) == 12288
+                && line.charAt(1) == 12288) {
+            String substring = line.substring(0, 2);
+            float cw = StaticLayout.getDesiredWidth(substring, getPaint());
+            canvas.drawText(substring, x + paddingLeft, mLineY, getPaint());
+            x += cw;
+            i += 2;
+        }
+        float d = (mViewWidth - lineWidth) / gapCount;
+        for (; i < line.length(); i++) {
+            String c = String.valueOf(line.charAt(i));
+            float cw = StaticLayout.getDesiredWidth(c, getPaint());
+            canvas.drawText(c, x + paddingLeft, mLineY, getPaint());
+            x += cw + d;
+        }
+    }
+
+    private boolean isFirstLineOfParagraph(int lineStart, String line) {
+        return line.length() > 3 && line.charAt(0) == ' '
+                && line.charAt(1) == ' ';
+    }
+
+    private boolean needScale(String line) {
+        if (line == null || line.length() == 0) {
+            return false;
+        } else {
+            return line.charAt(line.length() - 1) != '\n';
+        }
+    }
+
+}
