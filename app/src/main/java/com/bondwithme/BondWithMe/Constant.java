370c56a7595f9f4c82e473bb918cc7b982d56d25
weidong <heweidong@madxstudio.cn>
2015-08-17 17:53:24 +0800

    bond alert 添加了delete Recommend

--- app/src/main/java/com/bondwithme/BondWithMe/Constant.java
+++ app/src/main/java/com/bondwithme/BondWithMe/Constant.java
@@ -124,6 +124,7 @@ public class Constant {
     public static final String API_BONDALERT_GROUP = API_SERVER + "/bondwithme/index.php/api/bondAlertList/%s/module/group";
     public static final String API_BONDALERT_GROUP_CONFIRM = API_SERVER + "/bondwithme/index.php/api/confirmJoinGroup/%s";
     public static final String API_BONDALERT_GROUP_REJECT = API_SERVER + "/bondwithme/index.php/api/rejectJoinGroup/%s";
+    public static final String API_BONDALERT_REMOVE_RECOMMEND = API_SERVER + "/bondwithme/index.php/api/removeRecomUser/";
 
     /**
      * Api for sticker store
