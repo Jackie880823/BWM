4e38300b44c5b43f458393f22439065ca7aa2219
quan <quankun@madxstudio.cn>
2015-08-26 15:54:49 +0800

    修改消息64位

--- app/src/main/java/com/bondwithme/BondWithMe/util/AudioPlayUtils.java
+++ app/src/main/java/com/bondwithme/BondWithMe/util/AudioPlayUtils.java
@@ -29,6 +29,7 @@ public class AudioPlayUtils {
             return;
         }
         try {
+            mp.reset();
             mp.setDataSource(path);
             mp.prepare();
             mp.start();
