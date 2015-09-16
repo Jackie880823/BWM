fe238ae60e9740b0de218e728122ac3833ea96e3
ChristopherZhang <christopherzhang.cc@gmail.com>
2015-09-16 12:44:38 +0800

    tell  friends bug 修复。

--- app/src/main/java/com/bondwithme/BondWithMe/adapter/ContactCursorAdapter.java
+++ app/src/main/java/com/bondwithme/BondWithMe/adapter/ContactCursorAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.bondwithme.BondWithMe.R;
@@ -38,15 +40,15 @@ public class ContactCursorAdapter extends CursorRecyclerViewAdapter<ContactCurso
     public void onBindViewHolder(ViewHolder viewHolder, Cursor cursor) {
         viewHolder.contact_name.setText(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
         viewHolder.cb_selector.setTag(cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts._ID)) + "");
-
+        Log.d("", "onBindViewHolder" + cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts._ID)));
 //        if (selectContactIds.contains(viewHolder.getLayoutPosition() + "")) {
 //            viewHolder.cb_selector.setChecked(true);
 //        } else {
 //            viewHolder.cb_selector.setChecked(false);
 //        }
         boolean checked = false;
-        for (int selectPositon : selectContactIds) {
-            if (selectPositon == viewHolder.getLayoutPosition()) {
+        for (String selectPositon : contactsIds) {
+            if (selectPositon.equals(viewHolder.cb_selector.getTag() + "")) {
                 checked = true;
                 break;
             }
@@ -61,6 +63,7 @@ public class ContactCursorAdapter extends CursorRecyclerViewAdapter<ContactCurso
     }
 
     private List<Integer> selectContactIds = new ArrayList<>();
+    private List<String> contactsIds = new ArrayList<>();
 
     public List<Integer> getSelectContactIds() {
         return selectContactIds;
@@ -72,10 +75,10 @@ public class ContactCursorAdapter extends CursorRecyclerViewAdapter<ContactCurso
 
         public ViewHolder(View itemView) {
             super(itemView);
+
             cb_selector = (CheckBox) itemView.findViewById(R.id.cb_selector);
 
             contact_name = (TextView) itemView.findViewById(R.id.contact_name);
-
 
             cb_selector.setOnTouchListener(new View.OnTouchListener() {
                 @Override
@@ -84,18 +87,52 @@ public class ContactCursorAdapter extends CursorRecyclerViewAdapter<ContactCurso
                         try {
                             if (cb_selector.isChecked()) {
                                 selectContactIds.remove(getAdapterPosition());
+//                                boolean asd = contactsIds.remove(cb_selector.getTag() + "");
+//                                cb_selector.setChecked(false);
+//                                Log.d("", "onTouch " + cb_selector.getTag() + "--------" + contactsIds.size() + "--------" + asd +"--------" + contactsIds);
                             } else {
                                 selectContactIds.add(getAdapterPosition());
+//                                if (!contactsIds.contains(cb_selector.getTag() + ""))
+//                                {
+//                                    contactsIds.add(cb_selector.getTag() + "");
+//                                    cb_selector.setChecked(true);
+//                                }
                             }
                         } catch (Exception e) {
                         }
+
                     }
                     return false;
                 }
             });
 
+            //            christopher begin
+            cb_selector.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    try {
+                        if (isChecked) {
+                            if (!contactsIds.contains(cb_selector.getTag() + ""))
+                                contactsIds.add(cb_selector.getTag() + "");
+                            Log.d("", "onTouch " + cb_selector.getTag() + "--------" + contactsIds.size() + "--------" + contactsIds);
+                        } else {
+                            if (contactsIds.contains(cb_selector.getTag() + ""))
+                                contactsIds.remove(cb_selector.getTag() + "");
+                            Log.d("", "onTouch " + cb_selector.getTag() + "--------" + contactsIds.size() + "--------" + contactsIds);
+                        }
 
+                    } catch (Exception e) {
+
+                    }
+                }
+            });
+            //            christopher end
 
         }
+    }
+
+    public List<String> getContactsIds()
+    {
+        return contactsIds;
     }
 }
