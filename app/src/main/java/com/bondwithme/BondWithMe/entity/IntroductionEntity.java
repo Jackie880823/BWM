3fc87689b13fa9fa4d451573972f2a05d24f9db1
jackie <zhuweiping@madxstudio.cn>
2015-11-10 18:25:22 +0800

    Feature 第一次启动应用进入介绍页 添加介绍描述(英文语言)

---
+++ app/src/main/java/com/bondwithme/BondWithMe/entity/IntroductionEntity.java
@@ -0,0 +1,35 @@
+package com.bondwithme.BondWithMe.entity;
+
+import android.support.annotation.IdRes;
+
+import java.io.Serializable;
+
+/**
+ * Created 11/10/15.
+ * 介绍页面的实体，封装介绍页的描述和图片资源ID
+ * @author Jackie
+ * @version 1.0
+ */
+public class IntroductionEntity implements Serializable {
+    /**
+     * 图片资源ID
+     */
+    private int imagesResId;
+    private String description;
+
+    public int getImagesResId() {
+        return imagesResId;
+    }
+
+    public void setImagesResId(@IdRes int imagesResId) {
+        this.imagesResId = imagesResId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
