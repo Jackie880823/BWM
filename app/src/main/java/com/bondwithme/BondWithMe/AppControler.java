b35cb2d7066b0e5d84d82569caf0c1b0cd5dfa04
wing <wingzhong72@gmail.com>
2016-02-26 18:08:02 +0800

    .

--- app/src/main/java/com/bondwithme/BondWithMe/AppControler.java
+++ app/src/main/java/com/bondwithme/BondWithMe/AppControler.java
@@ -79,12 +79,14 @@ public class AppControler {
      * 结束所有Activity
      */
     public void finishAllActivity() {
-        for (int i = 0, size = activityStack.size(); i < size; i++) {
-            if (null != activityStack.get(i)) {
-                activityStack.get(i).finish();
+        if(activityStack!=null) {
+            for (int i = 0, size = activityStack.size(); i < size; i++) {
+                if (null != activityStack.get(i)) {
+                    activityStack.get(i).finish();
+                }
             }
+            activityStack.clear();
         }
-        activityStack.clear();
     }
 
     /**
