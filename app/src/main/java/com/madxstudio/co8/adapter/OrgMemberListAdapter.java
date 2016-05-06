3e6330dcd5b01b1de1c3460f19d36ab3bd22ccac
Jackie <jackie880823@gmail.com>
2016-05-06 10:22:37 +0800

    Fix: Admin中进入View Others中错误显示了管理员图标的问题

--- app/src/main/java/com/madxstudio/co8/adapter/OrgMemberListAdapter.java
+++ app/src/main/java/com/madxstudio/co8/adapter/OrgMemberListAdapter.java
@@ -17,7 +17,6 @@ import com.madxstudio.co8.R;
 import com.madxstudio.co8.entity.OrgMemberEntity;
 import com.madxstudio.co8.interfaces.NoFoundDataListener;
 import com.madxstudio.co8.ui.MainActivity;
-import com.madxstudio.co8.ui.OrgDetailActivity;
 import com.madxstudio.co8.util.MyTextUtil;
 import com.madxstudio.co8.util.PinYin4JUtil;
 import com.madxstudio.co8.widget.CircularNetworkImage;
@@ -100,28 +99,36 @@ public class OrgMemberListAdapter extends BaseAdapter implements Filterable {
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
+
         final OrgMemberEntity memberEntity = list.get(position);
         String userId = memberEntity.getUser_id();
         BitmapTools.getInstance(mContext).display(viewHolder.imageMain, String.format(Constant.API_GET_PHOTO, Constant.Module_profile, userId), R.drawable.default_head_icon, R.drawable.default_head_icon);
+
         if (!TextUtils.isEmpty(userId) && userId.equals(MainActivity.getUser().getUser_id()) && list.size() > 1) {
             viewHolder.meLine.setVisibility(View.VISIBLE);
         } else {
             viewHolder.meLine.setVisibility(View.GONE);
         }
+
         if (Constant.ORG_TRANSMIT_OTHER.equals(transmitData) && "0".equals(memberEntity.getAdded_flag())) {
+            // 添加好友没有获得准显示等待批准图标
             viewHolder.orgRequest.setVisibility(View.VISIBLE);
-        } else if ("1".equals(memberEntity.getAdmin_flag()) && mContext instanceof OrgDetailActivity) {
+            viewHolder.orgRequest.setImageResource(R.drawable.time);
+        } else if ("1".equals(memberEntity.getAdmin_flag()) && Constant.ADMIN_REQUEST.equals(requestType)) {
+            // 是管理员用户显示管理员图标
             viewHolder.orgRequest.setVisibility(View.VISIBLE);
             viewHolder.orgRequest.setImageResource(R.drawable.org_admin_icon);
         } else {
             viewHolder.orgRequest.setVisibility(View.GONE);
         }
+
         viewHolder.memberName.setText(memberEntity.getUser_given_name());
         if(Constant.ORG_TRANSMIT_OTHER.equals(transmitData)) {
             viewHolder.orgPosition.setText(memberEntity.getOrganisation());
         }else{
             viewHolder.orgPosition.setText(memberEntity.getPosition());
         }
+
         if (Constant.ADMIN_REQUEST.equals(requestType)) {
             View view = convertView.findViewById(R.id.iv_right);
             if (MainActivity.getUser().getUser_id().equals(memberEntity.getUser_id())) {
